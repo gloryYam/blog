85dfcdab82327ac2178fc6831b0b38a4c9ed7e8b
gloryYam <dudrhkd3892@gmail.com>
2024-01-10 20:43:53 +0900

    추가 디자인 변경

---
+++ front/src/router/index.ts
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/views/HomeView.vue'
+import WriteView from '@/views/views/WriteView.vue'
+import ReadView from '@/views/views/ReadView.vue'
+import EditView from '@/views/views/EditView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: "/write",
+      name: "write",
+      component: WriteView
+    },
+    {
+      path: "/read/:postId",
+      name: "read",
+      component: ReadView,
+      props: true,
+    },
+    {
+      path: "/edit/:postId",
+      name: "edit",
+      component: EditView,
+      props: true,
+    },
+
+
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/AboutView.vue')
+//     },
+  ],
+});
+
+export default router
---
+++ front/src/views/views/WriteView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {ref} from "vue"
+
+import axios from "axios";
+import {useRouter} from "vue-router"
+
+const title = ref("")     // 동적 변수
+const content = ref("")   // 동적 변수
+
+const router = useRouter();
+
+const write = function () {
+  axios
+      .post("/api/posts", {
+        title: title.value,
+        content: content.value
+      })
+      .then(() => {
+        router.replace({name: "home"});
+      });
+};
+</script>
+
+<template>
+  <!--글 작성 api -->
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력하세요."/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15"></el-input>
+  </div>
+
+  <div class="mt-2">
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
